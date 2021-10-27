55630ce17a094ffb2595acfb5249e1a61e3fdb76
Eric Quan <equan1090@gmail.com>
2021-10-27 09:13:31 -0600

    created homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { getAllVideoThunk } from "../../store/video";
+
+// import { getAllVideoThunk } from "../../store/video";
 
 const Homepage = () => {
     const dispatch = useDispatch()
     const user = useSelector((state) => state.session.user)
-    const [featuredVideo, setFeaturedVideo] = useState({})
-    const videos = useSelector((state) => state.videos)
+    const [featuredVideo, setFeaturedVideo] = useState(null)
+    // const videos = useSelector((state) => state.videos)
+    const [videos, setVideos] = useState({})
+    // useEffect(() => {
+    //     dispatch(getAllVideoThunk())
+    // }, [dispatch])
+
+
     useEffect(() => {
-        dispatch(getAllVideoThunk())
-    }, [dispatch])
+        (async () => {
+            const res = await fetch('/api/videos')
+            const videos = await res.json();
+            setVideos(videos);
+        })();
+    }, [])
+    console.log(videos)
+    let featured
+    if(videos.videos?.length > 0) {
+        featured = videos.videos?.reduce((prev, current) => {
+            return (prev.likes_count > current.likes_count) ? prev : current
+        })
+    }
+    console.log('this is featured',featured)
+
 
     return (
         <div>
-            <p>Hello</p>
+
         </div>
     )
 }
