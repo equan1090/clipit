8d5cde64eadb270c0941f2001f8b36b34d8df9a8
Eric Quan <equan1090@gmail.com>
2021-12-01 10:51:59 -0700

    added a likes store

---
+++ react-app/src/store/like.js
@@ -0,0 +1,59 @@
+const SET_LIKE = "SETLIKES"
+
+const load = (likes) => ({
+    type: SET_LIKE,
+    payload: likes
+})
+
+
+export const addLikeThunk = (like) => async (dispatch) => {
+    const res = await fetch("/api/videos/likes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(like)
+    })
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(load(data))
+    } else {
+        return "ADD LIKE FAILED"
+    }
+}
+
+export const deleteSingleLike = (id) => async (dispatch) => {
+    const res = await fetch("/api/videos/likes", {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+
+        body: JSON.stringify({id})
+    })
+    if (res.ok) {
+        const data = await res.json()
+        dispatch(load(data))
+    }
+}
+
+export const setAllLikes = () => async(dispatch) => {
+    const res = await fetch("/api/videos/likes")
+    if(res.ok) {
+        const data = await res.json()
+        dispatch(load(data))
+    }
+    else{
+        return "SETALLLIKES THUNK ERROR"
+    }
+}
+
+const initialState = []
+const likeReducer = (state = initialState, action) => {
+    let newState
+    switch(action.type) {
+        case SET_LIKE:
+            newState = action.payload.likes
+            return newState
+        default:
+            return state
+    }
+}
+
+export default likeReducer;
